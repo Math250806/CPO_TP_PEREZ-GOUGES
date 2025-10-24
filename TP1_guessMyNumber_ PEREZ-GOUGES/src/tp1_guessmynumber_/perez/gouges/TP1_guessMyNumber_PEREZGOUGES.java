eb9ee99f922f5f9063bb97ad3f18d89c0d5fd4b7
mathis <mathis@localhost>
2025-10-24 11:03:30 +0200

    Création d'un nouveau projet versionné

--- TP1_guessMyNumber_ PEREZ-GOUGES/src/tp1_guessmynumber_/perez/gouges/TP1_guessMyNumber_PEREZGOUGES.java
+++ TP1_guessMyNumber_ PEREZ-GOUGES/src/tp1_guessmynumber_/perez/gouges/TP1_guessMyNumber_PEREZGOUGES.java
@@ -4,17 +4,34 @@
  */
 package tp1_guessmynumber_.perez.gouges;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author mathis
  */
 public class TP1_guessMyNumber_PEREZGOUGES {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    Random generateurAleat = new Random(100);
+    int n = generateurAleat.nextInt(100);
+    System.out.println("Saisisez un nombre entre 0 et 100");
+    int nombre = sc.nextInt();  
+    
+    while (nombre != n) {
+    
+        if (nombre > n) {
+            System.out.println("Vous etes haut 😱");
+        } 
+    else if (nombre < n) {
+            System.out.println("Vous etes bas 😱");
+        } 
+    else {
+            System.out.println("Vous avez gagné 😱")
+        }
+    }
+    }    
 }

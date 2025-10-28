57936cf75d01b36a91152c952ad3312970e545fe
mathis <mathis@localhost>
2025-10-28 15:00:05 +0100

    Création d'un nouveau projet versionné

--- TP1_stats_PEREZ-GOUGES/src/tp1_stats_perez/gouges/TP1_stats_PEREZGOUGES.java
+++ TP1_stats_PEREZ-GOUGES/src/tp1_stats_perez/gouges/TP1_stats_PEREZGOUGES.java
@@ -4,6 +4,9 @@
  */
 package tp1_stats_perez.gouges;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author mathis
@@ -14,7 +17,30 @@ public class TP1_stats_PEREZGOUGES {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+     Scanner sc = new Scanner(System.in);
+        Random generateur = new Random();
+
+        // Tableau : 6 faces du dé
+        int[] des = new int[6];
+
+        // Nombre de lancers
+        System.out.println("Combien de fois veux-tu lancer le dé ?");
+        int m = sc.nextInt();
+
+        // Simulation des lancers
+        for (int i = 0; i < m; i++) {
+            int tirage = generateur.nextInt(6); 
+            des[tirage]++; 
+        }
+
+        //Résultats en %
+        System.out.println("Résultats des lancers en pourcentages :");
+        for (int i = 0; i < 6; i++) {
+            double pourcentage = ((double) des[i] / m) * 100; 
+            System.out.println("Face " + (i + 1) + " : " + pourcentage + " %");
+        }
+
+        sc.close();
     }
-    
 }

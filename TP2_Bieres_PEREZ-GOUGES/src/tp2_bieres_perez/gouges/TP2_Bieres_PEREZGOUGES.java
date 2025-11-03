7e743e713dbf16bc9244f45edbdfa284781e750e
mathis <mathis@localhost>
2025-11-03 15:22:17 +0100

    Création d'un nouveau projet versionné

--- TP2_Bieres_PEREZ-GOUGES/src/tp2_bieres_perez/gouges/TP2_Bieres_PEREZGOUGES.java
+++ TP2_Bieres_PEREZ-GOUGES/src/tp2_bieres_perez/gouges/TP2_Bieres_PEREZGOUGES.java
@@ -14,7 +14,14 @@ public class TP2_Bieres_PEREZGOUGES {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        // Création d'une bière avec le constructeur 3 paramètres
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0, "Dubuisson");
+        uneBiere.lireEtiquette();
+
+        // Création d'une seconde bière
+        BouteilleBiere secondeBiere = new BouteilleBiere("Leffe", 6.6, "Abbaye de Leffe");
+        secondeBiere.lireEtiquette();
     }
-    
+}
 }

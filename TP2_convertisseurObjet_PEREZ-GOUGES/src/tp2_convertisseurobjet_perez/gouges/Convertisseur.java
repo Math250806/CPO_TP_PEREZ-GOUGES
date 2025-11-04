f3b666a2896153c7fdeadc17ca4079080226efbd
mathis <mathis@localhost>
2025-11-04 11:11:51 +0100

    Création d'un nouveau projet versionné

---
+++ TP2_convertisseurObjet_PEREZ-GOUGES/src/tp2_convertisseurobjet_perez/gouges/Convertisseur.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_convertisseurobjet_perez.gouges;
+
+/**
+ *
+ * @author mathis
+ */
+    public class Convertisseur {
+
+    int nbConversions;
+
+    public Convertisseur() {
+        nbConversions = 0;
+    }
+
+    public double CelciusVersKelvin(double tCelcius) {
+        nbConversions++;
+        return tCelcius + 273.15;
+    }
+
+    public double KelvinVersCelcius(double tKelvin) {
+        nbConversions++;
+        return tKelvin - 273.15;
+    }
+
+    public double FarenheitVersCelcius(double tFarenheit) {
+        nbConversions++;
+        return (tFarenheit - 32) * 5 / 9;
+    }
+
+    public double CelciusVersFarenheit(double tCelcius) {
+        nbConversions++;
+        return tCelcius * 9 / 5 + 32;
+    }
+
+    public double FarenheitVersKelvin(double tFarenheit) {
+        nbConversions++;
+        return (tFarenheit - 32) * 5 / 9 + 273.15;
+    }
+
+    public double KelvinVersFarenheit(double tKelvin) {
+        nbConversions++;
+        return (tKelvin - 273.15) * 9 / 5 + 32;
+    }
+
+    @Override
+    public String toString() {
+        return "nb de conversions = " + nbConversions;
+    }
+}

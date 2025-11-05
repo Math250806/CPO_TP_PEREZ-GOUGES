a11a92fcfd5d3a8924edd17fee2272eedbc1d404
mathis <mathis@localhost>
2025-11-05 10:16:23 +0100

    Création d'un nouveau projet versionné

--- TP2_convertisseurObjet_PEREZ-GOUGES/src/tp2_convertisseurobjet_perez/gouges/TP2_convertisseurObjet_PEREZGOUGES.java
+++ TP2_convertisseurObjet_PEREZ-GOUGES/src/tp2_convertisseurobjet_perez/gouges/TP2_convertisseurObjet_PEREZGOUGES.java
@@ -4,17 +4,51 @@
  */
 package tp2_convertisseurobjet_perez.gouges;
 
-/**
- *
- * @author mathis
- */
+import java.util.Scanner;
+
 public class TP2_convertisseurObjet_PEREZGOUGES {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        Convertisseur conv = new Convertisseur();
+        Scanner sc = new Scanner(System.in);
+
+        int choix = -1;
+        double temp, resultat;
+
+        while (choix != 0) {
+            System.out.println("\nMENU CONVERTISSEUR");
+            System.out.println("1. Celsius -> Kelvin");
+            System.out.println("2. Kelvin -> Celsius");
+            System.out.println("3. Fahrenheit -> Celsius");
+            System.out.println("4. Celsius -> Fahrenheit");
+            System.out.println("5. Fahrenheit -> Kelvin");
+            System.out.println("6. Kelvin -> Fahrenheit");
+            System.out.println("0. Quitter");
+            System.out.print("Votre choix : ");
+
+            choix = sc.nextInt();
+
+            if (choix == 0) {
+                System.out.println("Merci d'avoir utilisé le convertisseur !");
+            } else if (choix >= 1 && choix <= 6) {
+                System.out.print("Entrez la température : ");
+                temp = sc.nextDouble();
+
+                if (choix == 1) resultat = conv.CelciusVersKelvin(temp);
+                else if (choix == 2) resultat = conv.KelvinVersCelcius(temp);
+                else if (choix == 3) resultat = conv.FarenheitVersCelcius(temp);
+                else if (choix == 4) resultat = conv.CelciusVersFarenheit(temp);
+                else if (choix == 5) resultat = conv.FarenheitVersKelvin(temp);
+                else resultat = conv.KelvinVersFarenheit(temp);
+
+                System.out.println("Résultat = " + resultat);
+                System.out.println(conv);
+            } else {
+                System.out.println("Choix invalide.");
+            }
+        }
+
+        sc.close();
     }
-    
 }

419c977508d60b80f8c3b5b86409f3909713213d
mathis <mathis@localhost>
2025-11-09 11:57:12 +0100

    Création d'un nouveau projet versionné

---
+++ TP2_manip_PEREZ-GOUGES/src/tp2_manip_perez/gouges/Moussaka.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_perez.gouges;
+
+/**
+ *
+ * @author mathis
+ */
+public class Moussaka {
+    int nbCalories;  
+    
+    // Constructeur
+    public Moussaka(int calories) {
+        nbCalories = calories;
+}
+}

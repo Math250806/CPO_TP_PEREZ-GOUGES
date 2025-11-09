71c4b102d848f60a96a4b074b80786b71e3496e0
mathis <mathis@localhost>
2025-11-09 11:56:59 +0100

    Création d'un nouveau projet versionné

--- TP2_manip_PEREZ-GOUGES/src/tp2_manip_perez/gouges/TP2_manip_PEREZGOUGES.java
+++ TP2_manip_PEREZ-GOUGES/src/tp2_manip_perez/gouges/TP2_manip_PEREZGOUGES.java
@@ -10,11 +10,40 @@ package tp2_manip_perez.gouges;
  */
 public class TP2_manip_PEREZGOUGES {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        
+        Tartiflette assiette1 = new Tartiflette(500);
+        Tartiflette assiette2 = new Tartiflette(600);
+        
+        Tartiflette assiette3 = assiette2;
+        assiette2.nbCalories += 50;
+                
     
+        System.out.println("Avant échange :");
+        System.out.println("assiette1 : " + assiette1.nbCalories);
+        System.out.println("assiette2 : " + assiette2.nbCalories);
+
+        // échange 
+        Tartiflette temp = assiette1;
+        assiette1 = assiette2;
+        assiette2 = temp;
+
+    
+        System.out.println("\nAprès échange :");
+        System.out.println("assiette1 : " + assiette1.nbCalories);
+        System.out.println("assiette2 : " + assiette2.nbCalories);
+        
+        // Déclaration d'un tableau de 10 références de Moussaka
+        Moussaka[] tableauMoussakas = new Moussaka[10];
+
+        // Création d'un objet Moussaka pour chaque référence
+        for (int i = 0; i < 10; i++) {
+            tableauMoussakas[i] = new Moussaka(500); 
+        }
+
+        System.out.println("\nTableau de Moussakas :");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Moussaka " + i + " : " + tableauMoussakas[i].nbCalories + " calories");
+        }
+    }
 }

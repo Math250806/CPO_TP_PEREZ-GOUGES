438c2340606824f5a07140018bd6e826e7cc6c3c
mathis <mathis@localhost>
2025-11-18 15:27:56 +0100

    Création d'un nouveau projet versionné

--- TP3_Heroic_Fantasy_PEREZ-GOUGES/src/tp3_heroic_fantasy_perez/gouges/Epee.java
+++ TP3_Heroic_Fantasy_PEREZ-GOUGES/src/tp3_heroic_fantasy_perez/gouges/Epee.java
@@ -9,7 +9,21 @@ package tp3_heroic_fantasy_perez.gouges;
  * @author mathis
  */
 public class Epee extends Arme {
-    public Epee(String nom, int niveauAttaque) {
+    private int finesse;
+
+    public Epee(String nom, int niveauAttaque, int finesse) {
         super(nom, niveauAttaque);
+        if (finesse < 0) finesse = 0;
+        if (finesse > 99) finesse = 99;
+        this.finesse = finesse;
     }
-}   
+
+    public int getFinesse() {
+        return finesse;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | Finesse : " + finesse;
+    }
+}

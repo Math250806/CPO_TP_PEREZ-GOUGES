8bf1ed22d2c6a0e662c891533d8728a0be1527da
mathis <mathis@localhost>
2025-12-05 15:31:36 +0100

    Changement de fichier : plus simple

---
+++ miniprojet_cadenas_PEREZ-GOUGES/src/miniprojet_cadenas_perez/gouges/Main.java
@@ -0,0 +1,19 @@
+package miniprojet_cadenas_perez.gouges;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author mathis
+ */
+public class Main {
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new Cadenas().setVisible(true);
+        });
+    }
+    
+}

0f7280e6424c0abddf3ffc43d73b976487d64e4d
mathis <mathis@localhost>
2025-12-17 10:33:44 +0100

    Début projet Light OFF

---
+++  LightOff_PEREZ-GOUGES_version_console/src/lightoff_perez/gouges_version_console/LightOff_PEREZGOUGES_version_console.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lightoff_perez.gouges_version_console;
+
+/**
+ *
+ * @author mathis
+ */
+public class LightOff_PEREZGOUGES_version_console {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

e65e7b08e812ab5229f54a6ad829498905f136ce
mathis <mathis@localhost>
2025-12-19 12:56:25 +0100

    PROJET QUI MACHREEEEEE AHHHHH 👹

--- LightOff_PEREZ-GOUGES_version_graphique/src/lightoff_perez/gouges_version_console/GrilleDeCellules.java
+++ LightOff_PEREZ-GOUGES_version_graphique/src/lightoff_perez/gouges_version_console/GrilleDeCellules.java
@@ -12,98 +12,81 @@ package lightoff_perez.gouges_version_console;
 import java.util.Random;
 
 public class GrilleDeCellules {
-    
-    private CelluleLumineuse[][] matriceCellules;
-    private int taille;
 
-    /**
-     * Constructeur : crée une grille taille x taille de cellules éteintes.
-     *
-     * @param taille taille de la grille
-     */
-    public GrilleDeCellules(int taille) {
-        this.taille = taille;
-        matriceCellules = new CelluleLumineuse[taille][taille];
+    private GrilleDeCellules grille;
 
-        for (int i = 0; i < taille; i++) {
-            for (int j = 0; j < taille; j++) {
-                matriceCellules[i][j] = new CelluleLumineuse();
-            }
-        }
+    public GrilleDeJeu(int taille) {
+        grille = new GrilleDeCellules(taille);
     }
 
     /**
-     * Active (inverse) toutes les cellules d'une ligne.
+     * Active toutes les cellules d'une ligne donnée.
      */
     public void activerLigneDeCellules(int idLigne) {
-        for (int j = 0; j < taille; j++) {
-            matriceCellules[idLigne][j].activerCellule();
-        }
+        grille.activerLigneDeCellules(idLigne);
     }
 
     /**
-     * Active (inverse) toutes les cellules d'une colonne.
+     * Active toutes les cellules d'une colonne donnée.
      */
     public void activerColonneDeCellules(int idColonne) {
-        for (int i = 0; i < taille; i++) {
-            matriceCellules[i][idColonne].activerCellule();
-        }
+        grille.activerColonneDeCellules(idColonne);
     }
 
     /**
-     * Active la diagonale descendante (↘).
+     * Active la diagonale descendante (du coin supérieur gauche au coin inférieur droit).
      */
     public void activerDiagonaleDescendante() {
-        for (int i = 0; i < taille; i++) {
-            matriceCellules[i][i].activerCellule();
-        }
+        grille.activerDiagonaleDescendante();
     }
 
     /**
-     * Active la diagonale montante (↗).
+     * Active la diagonale montante (du coin inférieur gauche au coin supérieur droit).
      */
     public void activerDiagonaleMontante() {
-        for (int i = 0; i < taille; i++) {
-            matriceCellules[i][taille - 1 - i].activerCellule();
-        }
+        grille.activerDiagonaleMontante();
+    }
+
+    /**
+     * Clique sur une cellule et inverse son état.
+     */
+    public void cliquerCellule(int ligne, int colonne) {
+        grille.getMatriceCellules()[ligne][colonne].changerEtat();
     }
 
     /**
      * Active aléatoirement une ligne, une colonne ou une diagonale.
      */
     public void activerLigneColonneOuDiagonaleAleatoire() {
-        Random rand = new Random();
-        int choix = rand.nextInt(4);
-
-        switch (choix) {
-            case 0 -> activerLigneDeCellules(rand.nextInt(taille));
-            case 1 -> activerColonneDeCellules(rand.nextInt(taille));
-            case 2 -> activerDiagonaleDescendante();
-            case 3 -> activerDiagonaleMontante();
-        }
+        grille.activerLigneColonneOuDiagonaleAleatoire();
     }
 
     /**
      * Mélange la grille en appliquant plusieurs activations aléatoires.
      */
     public void melangerMatriceAleatoirement(int nbTours) {
-        for (int i = 0; i < nbTours; i++) {
-            activerLigneColonneOuDiagonaleAleatoire();
-        }
+        grille.melangerMatriceAleatoirement(nbTours);
     }
 
     /**
-     * @return true si toutes les cellules sont éteintes
+     * Vérifie si toutes les cellules sont éteintes.
      */
     public boolean estEteinte() {
-        for (int i = 0; i < taille; i++) {
-            for (int j = 0; j < taille; j++) {
-                if (matriceCellules[i][j].estEteint()) {
-                    return false;
-                }
-            }
-        }
-        return true;
+        return grille.estEteinte();
+    }
+
+    /**
+     * Retourne la taille de la grille.
+     */
+    public int getTaille() {
+        return grille.getTaille();
+    }
+
+    /**
+     * Retourne la matrice de cellules.
+     */
+    public CelluleLumineuse[][] getMatriceCellules() {
+        return grille.getMatriceCellules();
     }
 
     /**
@@ -111,14 +94,6 @@ public class GrilleDeCellules {
      */
     @Override
     public String toString() {
-        String resultat = "";
-        for (int i = 0; i < taille; i++) {
-            for (int j = 0; j < taille; j++) {
-                resultat += matriceCellules[i][j] + " ";
-            }
-            resultat += "\n";
-        }
-        return resultat;
+        return grille.toString();
     }
-    
 }
